207811d79221544fb4cade4c7456a4135118d268
earcanal <paul@miraclefish.com>
2015-08-02 12:13:50 +0100

    Initial revision. Has lost some of the early "thinking out loud".

---
+++ panas.R
@@ -0,0 +1,38 @@
+#!/usr/bin/Rscript
+
+# the whole "meta" thing totally works!!!!
+# it captures the "thinking and doing process"
+# WATCH AND LEARN!
+# there's a *benefit* to watching when someone doesn't know how to do it
+# how do you *capture* that process and then present it speeded up?
+
+# (you can) write in concise, imperative english
+  # "self-documenting" code
+  # learn the syntax as you go
+
+
+datadir      <- '/media/paul/2E95-1293/study/participants'
+participants <- c(5:13,15,16,18,19)
+measures_f   <- paste(datadir,'/measures.csv',sep='')
+
+# read daily measures for all participants
+tbl <- tryCatch(read.csv(measures_f, header=TRUE),  error = function(err) {
+  cat(paste("Error opening",measures_f,':',err))
+})
+
+# calculate PANAS PA/NA totals
+# calculate means
+
+# all I new initially was that I needed to process every row for every participant
+for (participant in participants) {
+  #for session
+  # PA: Inspired, Alert, Excited, Enthusiastic, Determined + Sad, Depressed
+  # NA: Afraid, Upset, Nervous, Scared, Distressed + Anxious, Worried
+  # calculate means
+}
+
+# R Graphics Cookbook recipe 13.1
+"mcor <- cor(panas)
+round(mcor, digits=2)
+library(corrplot)"
+# ...
