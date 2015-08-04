9f6e2fdbdf833f3a24d0f415d15213bdbdec55e8
earcanal <paul@miraclefish.com>
2015-08-04 20:48:42 +0100

    Consume t-test output. Start to calculte Cohen's d.

--- prepost.R
+++ prepost.R
@@ -1,5 +1,10 @@
 #!/usr/bin/Rscript
 
+warnings()
+library(methods)
+library(lsr)
+library(plyr)
+
 options(width = 140)
 
 # read pre and post outcomes
@@ -8,9 +13,16 @@ participants <- c(5:13,15,16,18,19)
 
 read_data <- function(t) {
   data_f <- paste(datadir,t,'.csv',sep='')
-  data <- read.csv(data_f, header = TRUE, sep = ",", quote = "\"", dec = ".", fill = TRUE, comment.char = "")
+  data   <- read.csv(data_f, header = TRUE, sep = ",", quote = "\"", dec = ".", fill = TRUE, comment.char = "")
   # http://stackoverflow.com/questions/7112872/removing-specific-rows-from-a-dataframe
-  data[ data$participant %in% participants, ] # only real participant rows
+  data <- data[ data$participant %in% participants, ] # only real participant rows
+  rrs_items  <- sprintf("rrspre.SQ%03d.",1:22)
+  # FIXME: set pre/post columns when we have post data
+  data$rrspre   <- rowSums(subset(data,select = rrs_items))  # RRS total
+  pswq_items <- sprintf("pswqpre.SQ%03d.",1:16)
+  # FIXME: set pre/post columns when we have post data
+  data$pswqpre  <- rowSums(subset(data,select = pswq_items)) # PSWQ total
+  data
 }
 
 ## pre
@@ -18,31 +30,46 @@ pre <- read_data('pre')
 
 # recipe 12.2
 pre <- pre[order(pre$participant), ]
-rrs_items <- sprintf("rrspre.SQ%03d.",1:22)
-pre$rrs <- rowSums(subset(pre,select = rrs_items))   # RRS total
-pswq_items <- sprintf("pswqpre.SQ%03d.",1:16)
-pre$pswq <- rowSums(subset(pre,select = pswq_items)) # PSWQ total
-pre[c('participant','phq9pretotal','rrs','pswq','gad7pretotal')]
 
 ## post
-post <- read_data('post')
+pre_items  <- c('participant','rrspre','pswqpre','phq9pretotal','gad7pretotal')
+pre  <- subset(pre,select = pre_items)
+post <- pre
+# FIXME: uncomment the next line when we have some post data!
+# then replace pre data with real post data
+#post <- read_data('post')
+post <- rename(post, c("rrspre"="rrspost", 'pswqpre'='pswqpost', 'phq9pretotal'='phq9posttotal', 'gad7pretotal'='gad7posttotal'))
 
-# fill missing data with pre score i.e. 'no change'
+post_items <- c('participant',"rrspost",'pswqpost','phq9posttotal','gad7posttotal')
+prepost <- merge(pre, post, by='participant')
 
-# Yang et al. (2014) use equivalent 1-way ANOVA (F)
+## generate some random post data
+prepost$rrspost  <- sample(22:88,13)
+prepost$pswqpost <- sample(16:64,13)
+prepost$phq9posttotal <- sample(0:27,13)
+prepost$gad7posttotal <- sample(0:21,13)
+prepost
 
-# p < .05
+## t-tests (recipe 9.15)
 # Student's t-test: var.equal=TRUE
-#t.test(valence.mean ~ valence.category, nwords, var.equal=TRUE)
+#http://stackoverflow.com/questions/21840021/grabbing-certain-results-out-of-multiple-t-test-outputs-to-create-a-table
+tests <- list()
+tests[['RRS']] <- t.test(prepost$rrspre,prepost$rrspost, paired=TRUE, var.equal=TRUE)
+tests[['PSWQ']] <- t.test(prepost$pswqpre,prepost$pswqpost, paired=TRUE, var.equal=TRUE)
+tests[['PHQ-9']] <- t.test(prepost$phq9pretotal,prepost$phq9posttotal, paired=TRUE, var.equal=TRUE)
+tests[['GAD-7']] <- t.test(prepost$gad7pretotal,prepost$gad7posttotal, paired=TRUE, var.equal=TRUE)
 
-# Yang et al. (2014)
-# p > .05
-#t.test(arousal.mean ~ valence.category, nwords, var.equal=TRUE)
-#t.test(imageability.mean ~ valence.category, nwords, var.equal=TRUE)
-#t.test(familiarity.mean ~ valence.category, nwords, var.equal=TRUE)
+# need M(SD) for each
 
-# plus ...
-# p > .05
-#t.test(frequency ~ valence.category, nwords, var.equal=TRUE)
-#t.test(syllables ~ valence.category, nwords, var.equal=TRUE)
-#t.test(characters ~ valence.category, nwords, var.equal=TRUE)
+# extract your values using `sapply`
+df <- sapply(tests, function(x) {
+     c(x$estimate[1],
+       x$estimate[2],
+       ci.lower = x$conf.int[1],
+       ci.upper = x$conf.int[2],
+       x$parameter,
+       x$statistic,
+       p.value = x$p.value)
+})
+t(df)
+cohensD(prepost$rrspre, prepost$rrspost, method="paired")
