16ff0d42bb6ed4f1d569f14b2718ada1a1577315
earcanal <paul@miraclefish.com>
2015-08-12 23:10:26 +0100

    RTs table.

--- analyse.R
+++ analyse.R
@@ -1,6 +1,6 @@
 #!/usr/bin/Rscript
 
-# ---- grs ----
+# ---- inferentials ----
 
 options(error=traceback)
               # Single Case
@@ -17,7 +17,7 @@ ES        <- 'PND-' # expected effect is more negative i.e. increased avoidance 
 datadir <- '/media/paul/2E95-1293/study/participants/'
 setwd(datadir)
 participants <- c(5:13,15,16,18,19)
-participants <- c(5,7,8,9,10,11,12)
+participants <- c(5,6,7,8,9,10,11,12,15,16,18)
 
 # design properties
 design <- 'AB'
@@ -25,9 +25,146 @@ mt     <- 35 # FIXME: varies per P
 limit  <- 8
 
 ## Functions
-
 printf <- function(...) cat(sprintf(...))
 
+ylab <- list(i="I-Word Attentional Bias Score (ms)",n="N-Word Attentional Bias Score (ms)",grs="GRS Score",panas="I-PANAS-SF ++ Score")
+X11(type="cairo")
+
+## analyse daily outcomes for each participant
+p <- participants
+rt <- data.frame(participant=p,i_mean_a=p,i_sd_a=p,i_mean_b=p,i_sd_b=p,i_p=p,i_pnd=p,n_mean_a=p,n_sd_a=p,n_mean_b=p,n_sd_b=p,n_p=p,n_pnd=p)
+panas <- data.frame(participant=p,pa_a_mean=p,pa_a_sd=p,pa_b_mean=p,pa_b_sd=p,pa_p=p,pa_pnd=p,na_a_mean=p,na_a_sd=p,na_b_mean=p,na_b_sd=p,pa_b_p=p,pa_b_pnd=p)
+grs <- data.frame(participant=p,mean_a=p,sd_a=p,mean_b=p,sd_b=p,p=p,pnd=p)
+for (participant in participants) {
+  #printf("Participant %s\n",participant);
+  p_dir <- paste(datadir,participant,'/',sep='')
+  # FIXME: split PA/NA
+  for (dv in c('i','n','grs','panas')) {
+    #printf("DV: %s\n",dv);
+    dv_f <- paste(p_dir,'p',participant,'_',dv,'_scores',sep='')
+    # generate plot for visual analysis
+    #graph.TREND(design,'LSR','mean',data=read.table(iwords),xlab="Measurement Times",ylab="Attentional Bias Score (I-word pairs)")
+    data <- read.table(dv_f)
+    #graph.CL(design,'mean',data=data,xlab="Measurement Times",ylab=ylab[[dv]])
+    #savePlot(filename=paste(p_dir,'p',participant,'_',dv,'.jpg',sep=''), type='jpeg')
+    p   <- pvalue.systematic(design,statistic,save = "no",limit = limit, data = data)
+    pnd <- ES(design,ES,data = data)
+    # FIXME: meta analysis
+    # caclulate mean and sd for phases A and B
+    a      <- data[data$V1 == 'A','V2']
+    mean_a <- mean(a)
+    sd_a   <- sd(a)
+    b      <- data[data$V1 == 'B','V2']
+    mean_b <- mean(b)
+    sd_b   <- sd(b)
+    if (dv == 'i' | dv == 'n') { # RTs table
+      rt[rt$participant == participant,paste(dv,'_','mean_a',sep='')] <- mean_a
+      rt[rt$participant == participant,paste(dv,'_','sd_a',sep='')]   <- sd_a
+      rt[rt$participant == participant,paste(dv,'_','mean_b',sep='')] <- mean_b
+      rt[rt$participant == participant,paste(dv,'_','sd_b',sep='')]   <- sd_b
+      rt[rt$participant == participant,paste(dv,'_','p',sep='')]      <- p
+      rt[rt$participant == participant,paste(dv,'_','pnd',sep='')]    <- pnd
+    } else if (dv == 'panas') {  # PANAS table FIXME: should be 'pa' | 'na'
+    } else {                     # GRS table
+      grs[grs$participant == participant,'p']      <- p
+      grs[grs$participant == participant,'pnd']    <- pnd
+      grs[grs$participant == participant,'mean_a'] <- mean_a
+      grs[grs$participant == participant,'sd_a']   <- sd_a
+      grs[grs$participant == participant,'mean_b'] <- mean_b
+      grs[grs$participant == participant,'sd_b']   <- sd_b
+    }
+  }
+}
+
+# merge sessions completed into tables
+schedules_f <- paste(datadir,'rumination study - schedules.csv',sep='')
+schedules   <- read.csv(schedules_f,as.is=TRUE) # ignore non-numerics in Participants column
+sessions    <- schedules[schedules$'Participant' %in% participants,c('Participant','Complete')]
+sessions    <- rename(sessions,c('Participant'='participant','Complete'='sessions'))
+sessions[,'participant'] <- sapply(sessions[,'participant'], as.numeric)
+rt    <- merge(sessions,rt,by='participant')
+rt    <- rt[with(rt, order(participant)), ]
+grs   <- merge(sessions,grs,by='participant')
+grs   <- grs[with(grs, order(participant)), ]
+panas <- merge(sessions,panas,by='participant')
+panas <- panas[with(panas, order(participant)), ]
+
+pvalue <- function(p) {
+  p <- as.numeric(p)
+  if (round(p, digits=2) < 0.001) {
+    p <- '< .001'
+  } else {
+    p <- sprintf("%0.3f",p)
+    p <- str_replace(as.character(p), "^0\\.", ".")
+  }
+  p
+}
+
+format_grs <- function(x) {
+  x['p']   <- pvalue(x['p'])
+  x['pnd'] <- sprintf("%0.2f",as.numeric(x['pnd']))
+  x['a']   <- sprintf("%0.2f(%0.2f)",as.numeric(x['mean_a']),as.numeric(x['sd_a']))
+  x['b']   <- sprintf("%0.2f(%0.2f)",as.numeric(x['mean_b']),as.numeric(x['sd_b']))
+  x
+}
+
+format_rt <- function(x) {
+  x['i_p']   <- pvalue(x['i_p'])
+  x['i_pnd'] <- sprintf("%0.2f",as.numeric(x['i_pnd']))
+  x['i_a']   <- sprintf("%0.2f(%0.2f)",as.numeric(x['i_mean_a']),as.numeric(x['i_sd_a']))
+  x['i_b']   <- sprintf("%0.2f(%0.2f)",as.numeric(x['i_mean_b']),as.numeric(x['i_sd_b']))
+  x['n_p']   <- pvalue(x['n_p'])
+  x['n_pnd'] <- sprintf("%0.2f",as.numeric(x['n_pnd']))
+  x['n_a']   <- sprintf("%0.2f(%0.2f)",as.numeric(x['n_mean_a']),as.numeric(x['n_sd_a']))
+  x['n_b']   <- sprintf("%0.2f(%0.2f)",as.numeric(x['n_mean_b']),as.numeric(x['n_sd_b']))
+  x
+}
+
+## RT table
+results <- apply(rt,1,format_rt)
+results <- t(results)
+results <- subset(results, select=c(participant,sessions,i_a,i_b,i_p,i_pnd,n_a,n_b,n_p,n_pnd))
+strCaption <- paste0("RT inferentials")
+print(xtable(results, caption=strCaption, label="rt", align=c('c','c','c','l','l','r','r','l','l','r','r')),
+      size="footnotesize",
+      include.rownames=FALSE,
+      include.colnames=FALSE,
+      floating.environment='sidewaystable',
+      caption.placement="top",
+      hline.after=NULL,
+      add.to.row = list(pos = list(-1, nrow(results)),
+                        command = c(paste("\\toprule \n",
+					  "& & I words & & & & N words\\\\\n",
+                                          "\\cline{3-8} \n",
+					  "& & A & B & & & A & B\\\\\n",
+                                          "\\cline{3-10} \n",
+                                          "Participant & Sessions & ${M}$(${SD}$) & ${M}$(${SD}$) & rand ${p}$ & ${PND}$ & ${M}$(${SD}$) & ${M}$(${SD}$) & rand ${p}$ & ${PND}$\\\\\n",
+                                          "\\midrule \n"),
+                                          "\\bottomrule \n")
+				    )
+		      )
+
+## GRS table
+results <- apply(grs,1,format_grs)
+results <- t(results)
+results <- subset(results, select=c(participant,sessions,a,b,p,pnd))
+strCaption <- paste0("GRS inferentials")
+print(xtable(results, caption=strCaption, label="grs", align=c('c','c','c','l','l','r','r')),
+      size="footnotesize",
+      include.rownames=FALSE,
+      include.colnames=FALSE,
+      caption.placement="top",
+      hline.after=NULL,
+      add.to.row = list(pos = list(-1, nrow(results)),
+                        command = c(paste("\\toprule \n",
+					  "& & A & B \\\\\n",
+                                          "\\cline{3-4} \n",
+                                          "Participant & Sessions &
+					  ${M}$(${SD}$) & ${M}$(${SD}$) & rand ${p}$ & ${PND}$\\\\\n",
+                                          "\\midrule \n"),
+                                          "\\bottomrule \n")
+				    )
+		      )
 # MBD
 # GRS
 mbd <- function() {
@@ -52,103 +189,7 @@ mbd <- function() {
   }
 }
 
-##
-
 #mbd()
-
-ylab <- list(i="I-Word Attentional Bias Score (ms)",n="N-Word Attentional Bias Score (ms)",grs="GRS Score",panas="I-PANAS-SF ++ Score")
-#X11(type="cairo")
-
-## analyse daily outcomes for each participant
-p <- participants
-rt <- data.frame(participant=p,sessions=p,i_a_mean=p,i_a_sd=p,i_b_mean=p,i_b_sd=p,i_p=p,i_pnd=p,n_a_mean=p,n_a_sd=p,n_b_mean=p,n_b_sd=p,n_p=p,n_pnd=p)
-panas <- data.frame(participant=p,sessions=p,pa_a_mean=p,pa_a_sd=p,pa_b_mean=p,pa_b_sd=p,pa_p=p,pa_pnd=p,na_a_mean=p,na_a_sd=p,na_b_mean=p,na_b_sd=p,pa_b_p=p,pa_b_pnd=p)
-grs <- data.frame(participant=p,mean_a=p,sd_a=p,mean_b=p,sd_b=p,p=p,pnd=p)
-for (participant in participants) {
-  #printf("Participant %s\n",participant);
-  p_dir <- paste(datadir,participant,'/',sep='')
-  # FIXME: split PA/NA
-  for (dv in c('i','n','grs','panas')) {
-    #printf("DV: %s\n",dv);
-    dv_f <- paste(p_dir,'p',participant,'_',dv,'_scores',sep='')
-    # generate plot for visual analysis
-    #graph.TREND(design,'LSR','mean',data=read.table(iwords),xlab="Measurement Times",ylab="Attentional Bias Score (I-word pairs)")
-    data <- read.table(dv_f)
-    #graph.CL(design,'mean',data=data,xlab="Measurement Times",ylab=ylab[[dv]])
-    #savePlot(filename=paste(p_dir,'p',participant,'_',dv,'.jpg',sep=''), type='jpeg')
-    p   <- pvalue.systematic(design,statistic,save = "no",limit = limit, data = data)
-    pnd <- ES(design,ES,data = data)
-    # caclulate mean and sd for phases A and B
-    a      <- data[data$V1 == 'A','V2']
-    mean_a <- mean(a)
-    sd_a   <- sd(a)
-    b      <- data[data$V1 == 'B','V2']
-    mean_b <- mean(b)
-    sd_b   <- sd(b)
-    if (dv == 'i' | dv == 'n') { # RTs table
-    } else if (dv == 'panas') {  # PANAS table FIXME: should be 'pa' | 'na'
-    } else {                     # GRS table
-      grs[grs$participant == participant,'p']      <- p
-      grs[grs$participant == participant,'pnd']    <- pnd
-      grs[grs$participant == participant,'mean_a'] <- mean_a
-      grs[grs$participant == participant,'sd_a']   <- sd_a
-      grs[grs$participant == participant,'mean_b'] <- mean_b
-      grs[grs$participant == participant,'sd_b']   <- sd_b
-    }
-  }
-}
-
-# merge sessions completed into tables
-schedules_f <- paste(datadir,'rumination study - schedules.csv',sep='')
-schedules   <- read.csv(schedules_f,as.is=TRUE) # ignore non-numerics in Participants column
-sessions    <- schedules[schedules$'Participant' %in% participants,c('Participant','Complete')]
-sessions    <- rename(sessions,c('Participant'='participant','Complete'='sessions'))
-sessions[,'participant'] <- sapply(sessions[,'participant'], as.numeric)
-#rt    <- merge(sessions,rt,by='participant')
-#rt    <- rt[with(rt, order(participant,sessions)), ]
-grs   <- merge(sessions,grs,by='participant')
-grs   <- grs[with(grs, order(participant,sessions)), ]
-#panas <- merge(sessions,panas,by='participant')
-#panas <- panas[with(panas, order(participant,sessions)), ]
-
-format <- function(x) {
-  # FIXME: some tables have multiple values
-  p <- as.numeric(x['p'])
-  if (round(p, digits=2) < 0.001) {
-    p <- '< .001'
-  } else {
-    p <- sprintf("%0.3f",p)
-    p <- str_replace(as.character(p), "^0\\.", ".")
-  }
-  x['p']   <- p
-  x['pnd'] <- sprintf("%0.2f",as.numeric(x['pnd']))
-  x['a']   <- sprintf("%0.2f(%0.2f)",as.numeric(x['mean_a']),as.numeric(x['sd_a']))
-  x['b']   <- sprintf("%0.2f(%0.2f)",as.numeric(x['mean_b']),as.numeric(x['sd_b']))
-  x
-}
-
-
-results <- apply(grs,1,format)
-results <- t(results)
-results <- subset(results, select=c(participant,sessions,a,b,p,pnd))
-
-strCaption <- paste0("\\textbf{Table n} GRS inferentials")
-print(xtable(results, caption=strCaption, label="prepost", align=c('c','c','l','l','r','r','r')),
-      size="footnotesize",
-      include.rownames=FALSE,
-      include.colnames=FALSE,
-      caption.placement="top",
-      hline.after=NULL,
-      add.to.row = list(pos = list(-1, nrow(results)),
-                        command = c(paste("\\toprule \n",
-					  "& & A & B \\\\\n",
-                                          "\\cline{3-4} \n",
-                                          "Participant & Sessions &
-					  ${M}$(${SD}$) & ${M}$(${SD}$) & rand ${p}$ & ${PND}$\\\\\n",
-                                          "\\midrule \n"),
-                                          "\\bottomrule \n")
-				    )
-		      )
 
 make_schedule <- function() {
   #quantity(design="AB",MT=15,limit=6)
