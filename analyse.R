e26e0cb3d5559de2ed8bb991ac2c7a084fcfad7f
earcanal <paul@miraclefish.com>
2015-08-13 16:17:21 +0100

    PANAS inferentials table.

--- analyse.R
+++ analyse.R
@@ -33,20 +33,20 @@ X11(type="cairo")
 ## analyse daily outcomes for each participant
 p <- participants
 rt <- data.frame(participant=p,i_mean_a=p,i_sd_a=p,i_mean_b=p,i_sd_b=p,i_p=p,i_pnd=p,n_mean_a=p,n_sd_a=p,n_mean_b=p,n_sd_b=p,n_p=p,n_pnd=p)
-panas <- data.frame(participant=p,pa_a_mean=p,pa_a_sd=p,pa_b_mean=p,pa_b_sd=p,pa_p=p,pa_pnd=p,na_a_mean=p,na_a_sd=p,na_b_mean=p,na_b_sd=p,pa_b_p=p,pa_b_pnd=p)
+panas <- data.frame(participant=p,pa_mean_a=p,pa_sd_a=p,pa_mean_b=p,pa_sd_b=p,pa_p=p,pa_pnd=p,na_mean_a=p,na_sd_a=p,na_mean_b=p,na_sd_b=p,na_p=p,na_pnd=p,d_mean_a=p,d_sd_a=p,d_mean_b=p,d_sd_b=p,d_p=p,d_pnd=p)
 grs <- data.frame(participant=p,mean_a=p,sd_a=p,mean_b=p,sd_b=p,p=p,pnd=p)
 for (participant in participants) {
   #printf("Participant %s\n",participant);
   p_dir <- paste(datadir,participant,'/',sep='')
   # FIXME: split PA/NA
-  for (dv in c('i','n','grs','panas')) {
+  for (dv in c('i','n','grs','pa','na','d')) {
     #printf("DV: %s\n",dv);
     dv_f <- paste(p_dir,'p',participant,'_',dv,'_scores',sep='')
     # generate plot for visual analysis
     #graph.TREND(design,'LSR','mean',data=read.table(iwords),xlab="Measurement Times",ylab="Attentional Bias Score (I-word pairs)")
     data <- read.table(dv_f)
-    #graph.CL(design,'mean',data=data,xlab="Measurement Times",ylab=ylab[[dv]])
-    #savePlot(filename=paste(p_dir,'p',participant,'_',dv,'.jpg',sep=''), type='jpeg')
+    graph.CL(design,'mean',data=data,xlab="Measurement Times",ylab=ylab[[dv]])
+    savePlot(filename=paste(p_dir,'p',participant,'_',dv,'.jpg',sep=''), type='jpeg')
     p   <- pvalue.systematic(design,statistic,save = "no",limit = limit, data = data)
     pnd <- ES(design,ES,data = data)
     # FIXME: meta analysis
@@ -57,15 +57,21 @@ for (participant in participants) {
     b      <- data[data$V1 == 'B','V2']
     mean_b <- mean(b)
     sd_b   <- sd(b)
-    if (dv == 'i' | dv == 'n') { # RTs table
+    if (dv == 'i' | dv == 'n') {                       # RTs table
       rt[rt$participant == participant,paste(dv,'_','mean_a',sep='')] <- mean_a
       rt[rt$participant == participant,paste(dv,'_','sd_a',sep='')]   <- sd_a
       rt[rt$participant == participant,paste(dv,'_','mean_b',sep='')] <- mean_b
       rt[rt$participant == participant,paste(dv,'_','sd_b',sep='')]   <- sd_b
       rt[rt$participant == participant,paste(dv,'_','p',sep='')]      <- p
       rt[rt$participant == participant,paste(dv,'_','pnd',sep='')]    <- pnd
-    } else if (dv == 'panas') {  # PANAS table FIXME: should be 'pa' | 'na'
-    } else {                     # GRS table
+    } else if (dv == 'pa' | dv == 'na' | dv == 'd') {  # PANAS table
+      panas[panas$participant == participant,paste(dv,'_','mean_a',sep='')] <- mean_a
+      panas[panas$participant == participant,paste(dv,'_','sd_a',sep='')]   <- sd_a
+      panas[panas$participant == participant,paste(dv,'_','mean_b',sep='')] <- mean_b
+      panas[panas$participant == participant,paste(dv,'_','sd_b',sep='')]   <- sd_b
+      panas[panas$participant == participant,paste(dv,'_','p',sep='')]      <- p
+      panas[panas$participant == participant,paste(dv,'_','pnd',sep='')]    <- pnd
+    } else {                                           # GRS table
       grs[grs$participant == participant,'p']      <- p
       grs[grs$participant == participant,'pnd']    <- pnd
       grs[grs$participant == participant,'mean_a'] <- mean_a
@@ -119,6 +125,46 @@ format_rt <- function(x) {
   x['n_b']   <- sprintf("%0.2f(%0.2f)",as.numeric(x['n_mean_b']),as.numeric(x['n_sd_b']))
   x
 }
+
+format_panas <- function(x) {
+  x['pa_p']   <- pvalue(x['pa_p'])
+  x['pa_pnd'] <- sprintf("%0.2f",as.numeric(x['pa_pnd']))
+  x['pa_a']   <- sprintf("%0.2f(%0.2f)",as.numeric(x['pa_mean_a']),as.numeric(x['pa_sd_a']))
+  x['pa_b']   <- sprintf("%0.2f(%0.2f)",as.numeric(x['pa_mean_b']),as.numeric(x['pa_sd_b']))
+  x['na_p']   <- pvalue(x['na_p'])
+  x['na_pnd'] <- sprintf("%0.2f",as.numeric(x['na_pnd']))
+  x['na_a']   <- sprintf("%0.2f(%0.2f)",as.numeric(x['na_mean_a']),as.numeric(x['na_sd_a']))
+  x['na_b']   <- sprintf("%0.2f(%0.2f)",as.numeric(x['na_mean_b']),as.numeric(x['na_sd_b']))
+  x['d_p']    <- pvalue(x['d_p'])
+  x['d_pnd']  <- sprintf("%0.2f",as.numeric(x['d_pnd']))
+  x['d_a']    <- sprintf("%0.2f(%0.2f)",as.numeric(x['d_mean_a']),as.numeric(x['d_sd_a']))
+  x['d_b']    <- sprintf("%0.2f(%0.2f)",as.numeric(x['d_mean_b']),as.numeric(x['d_sd_b']))
+  x
+}
+
+## PANAS table
+results <- apply(panas,1,format_panas)
+results <- t(results)
+results <- subset(results, select=c(participant,sessions,pa_a,pa_b,pa_p,pa_pnd,na_a,na_b,na_p,na_pnd,d_a,d_b,d_p,d_pnd))
+strCaption <- paste0("I-PANAS-SF+sad+depressed inferentials")
+print(xtable(results, caption=strCaption, label="panas", align=c('c','c','c','l','l','r','r','l','l','r','r','l','l','r','r')),
+      size="footnotesize",
+      include.rownames=FALSE,
+      include.colnames=FALSE,
+      floating.environment='sidewaystable',
+      caption.placement="top",
+      hline.after=NULL,
+      add.to.row = list(pos = list(-1, nrow(results)),
+                        command = c(paste("\\toprule \n",
+					  "& & Positive Affect (PA) & & & & Negative Affect (NA) & & & & 'Depression' ('sad','depressed')\\\\\n",
+                                          "\\cline{3-14} \n",
+					  "& & A & B & & & A & B & & & A & B\\\\\n",
+                                          "\\cline{3-14} \n",
+                                          "Participant & Sessions & ${M}$(${SD}$) & ${M}$(${SD}$) & rand ${p}$ & ${PND}$ & ${M}$(${SD}$) & ${M}$(${SD}$) & rand ${p}$ & ${PND}$ & ${M}$(${SD}$) & ${M}$(${SD}$) & rand ${p}$ & ${PND}$\\\\\n",
+                                          "\\midrule \n"),
+                                          "\\bottomrule \n")
+				    )
+		      )
 
 ## RT table
 results <- apply(rt,1,format_rt)
